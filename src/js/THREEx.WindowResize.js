50e19473f3c9d973300eaaa1b29bc90bca12c067
DatNT1507 <nguyenthanhdat15072004@gmail.com>
2025-04-17 17:03:50 +0700

    init game

---
+++ src/js/THREEx.WindowResize.js
@@ -0,0 +1,29 @@
+/** @namespace */
+var THREEx	= THREEx 		|| {};
+
+/**
+ * Update renderer and camera when the window is resized
+ * 
+ * @param {Object} renderer the renderer to update
+ * @param {Object} Camera the camera to update
+*/
+THREEx.WindowResize	= function(renderer, camera){
+	var callback	= function(){
+		// notify the renderer of the size change
+		renderer.setSize( window.innerWidth, window.innerHeight );
+		// update the camera
+		camera.aspect	= window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+	}
+	// bind the resize event
+	window.addEventListener('resize', callback, false);
+	// return .stop() the function to stop watching window resize
+	return {
+		/**
+		 * Stop watching window resize
+		*/
+		stop	: function(){
+			window.removeEventListener('resize', callback);
+		}
+	};
+}
